eb97870e2c8da140ef44d8028d0705a2ea77e06a
Kishore kumar <kishore3886@gmail.com>
2020-11-17 14:30:40 -0800

    new dash board and other functions

---
+++ ZeroTouchHR/Pages/EmployeeList/DashBoard.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ZeroTouchHR.Models;
+
+namespace ZeroTouchHR.Pages.EmployeeList
+{
+    public class DashBoardModel : PageModel
+    {
+
+        private readonly ILogger<DashBoardModel> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public DashBoardModel(ILogger<DashBoardModel> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public Employee employee { get; set; }
+        public void OnGet()
+        {
+
+            ViewData["StartedCount"] = _db.employee.Where(e => e.Status == "Started").Count();
+            ViewData["InprocessCount"] = _db.employee.Where(e => e.Status == "Inprocess").Count();
+            ViewData["CompletedCount"] = _db.employee.Where(e => e.Status == "Completed").Count();
+
+        }
+
+
+
+
+    }
+}
---
+++ ZeroTouchHR/Pages/User/Index.cshtml.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZeroTouchHR.Models;
+
+namespace ZeroTouchHR.Pages.User
+{
+    public class IndexModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+
+        [BindProperty]
+        public Employee employee { get; set; }
+        public async Task OnGet(int id)
+        {
+
+            employee = await _db.employee.FindAsync(id);
+
+
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var EmpFromDb = await _db.employee.FindAsync(employee.id);
+
+                EmpFromDb.FName = employee.FName;
+                EmpFromDb.LName = employee.LName;
+                EmpFromDb.title = employee.title;
+                EmpFromDb.AddressLine1 = employee.AddressLine1;
+                EmpFromDb.AddressLine2 = employee.AddressLine2;
+                EmpFromDb.PhoneNumber = employee.PhoneNumber;
+                EmpFromDb.State = employee.State;
+                EmpFromDb.City = employee.City;
+                EmpFromDb.Zip = employee.Zip;
+                EmpFromDb.Email = employee.Email;
+                EmpFromDb.Password = employee.Password;
+                EmpFromDb.Status = "Started";
+                //employee.Status = "Started";
+                //await _db.employee.AddAsync(employee);
+                //await _db.SaveChangesAsync();
+                //return RedirectToPage("Index");
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+
+        }
+
+    }
+}
