a1e95cb4ee89771bd754c7fe40f1d02365fe7491
Kishore kumar <kishore3886@gmail.com>
2020-11-22 06:37:11 -0800

    added new thankyou to create s3 folder
    created couple of new enhancements to the application Ui level and application functionality. s3 folder creation and upload document code.(It has an error to fix. and User index page has both codes, first one is with employee object and next one is related to the new object to carry the photo)

--- ZeroTouchHR/Models/Employee.cs
+++ ZeroTouchHR/Models/Employee.cs
@@ -1,7 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace ZeroTouchHR.Models
 {
+    //public class Employee
+    //{
+    //    [Key]
+    //    public int id { get; set; }
+    //    [Required]
+    //    public string FName { get; set; }
+    //    public string LName { get; set; }
+
+    //    public string PhoneNumber { get; set; }
+
+    //    public string title { get; set; }
+
+    //    public string AddressLine1 { get; set; }
+    //    public string AddressLine2 { get; set; }
+
+    //    public string City { get; set; }
+
+    //    public string State { get; set; }
+    //    public string Zip { get; set; }
+    //    [Required]
+    //    public string Email { get; set; }
+    //    [Required]
+    //    public string Password { get; set; }
+
+    //    public string Status { get; set; }
+
+    //    [Required]
+    //    public string UserName { get; set; }
+
+    //}
+
     public class Employee
     {
         [Key]
