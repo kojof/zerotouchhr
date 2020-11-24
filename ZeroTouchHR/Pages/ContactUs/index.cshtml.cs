d621c83dabf7a12e26c6da72017d5118de2fadf3
kojo frempong <kojof@hotmail.com>
2020-11-24 21:28:54 +0000

    changes to contact us

---
+++ ZeroTouchHR/Pages/ContactUs/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZeroTouchHR.Pages.ContactUs
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
